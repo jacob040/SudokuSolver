94826023b2c42d5ada0cb7d39aed6b51bb62012c
jacob040 <jonssonjacob18@gmail.com>
2024-03-08 18:44:02 +0100

    SQLAlchemy setup and models

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "sudokudb.db"
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET KEY'] = 'Jacob is a swaggy dude'
-    
+    #app.config['SECRET KEY'] = 'Jacob is a swaggy dude'
+    app.secret_key = 'Jacob is a swaggy dude'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
     from .views import views
     from .auth import auth
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    from .models import User, Score
+    
+    with app.app_context():
+        db.create_all()
+            
     return app
+
     
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,45 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+
 
 auth = Blueprint('auth', __name__) 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True) #Passes value True as boolean variable to login.html
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout<p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error')
+        elif len(firstName) < 3:
+            flash('First name must be greater than 2 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be atleast 7 characters', category='error')
+        else:
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+        
     return render_template("sign_up.html")
+
+#routes need to know what type of http request we sent because based on the type of request they will act differently
+#POST request usually mean that you make a change/create to your db or website or whatever.
