f1a50369c632f1f8ecc948b32e3a022c7f5c5395
jacob040 <jonssonjacob18@gmail.com>
2024-03-09 13:38:15 +0100

    Final groundwork, login functionality

--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user   #current_user related to UserMixin
 
 views = Blueprint('views', __name__) #"Blueprint for our flask application"
 
 @views.route('/')
+@login_required #Makes homepage unavailible unless logged in
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
