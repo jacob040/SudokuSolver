962d7b4932737c2873fb3013c37570c4a1a00a57
jacob040 <jonssonjacob18@gmail.com>
2024-04-02 22:24:03 +0200

    committing local changes before merge

--- website/static/sudoku.js
+++ website/static/sudoku.js
@@ -31,7 +31,16 @@ function setGame() {
         for (let c = 0; c < 9; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
-
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }   
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
             tile.addEventListener("click", function () {  //"CREATES" AND "ASSIGNS" AN EVENTLISTENER TO EACH TILE DURING INSTATIATION, LISTENING FOR A CLICK
                 if(selectedTile !== "") {
                     document.getElementById(selectedTile).classList.remove("highlight");
@@ -44,6 +53,7 @@ function setGame() {
             });
 
             tile.classList.add("tile");
+
             document.getElementById("board").append(tile);
         }
     }
